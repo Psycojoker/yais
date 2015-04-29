43830b336b070de777875d9d8a3cdb0205b72721
Laurent Peuch <cortex@worlddomination.be>
2015-04-30 00:41:06 +0200

    [enh] beeeeeeuuuuuuuuuaaaaaaa encoding sucks, let's always work in utf8 becauses bytes sucks

--- yais.py
+++ yais.py
@@ -28,15 +28,16 @@ class User():
     @asyncio.coroutine
     def loop(self):
         try:
-            data = (yield from self.reader.readline())
-            print(data)
+            data = yield from self.get_next_line()
+
             proto, nick = data.rstrip().split()
-            assert proto == b"NICK"
+            assert proto == "NICK"
             self.nick = nick
-            data = (yield from self.reader.readline())
-            print(data)
-            proto, realname = data.rstrip().split(b" ", 1)
-            assert proto == b"USER"
+
+            data = yield from self.get_next_line()
+
+            proto, realname = data.rstrip().split(" ", 1)
+            assert proto == "USER"
             self.send(":%s MODE %s :+i" % (nick, nick))
 
             self.send(":myself 001 %s :Hello honey" % nick)
@@ -45,32 +46,47 @@ class User():
             self.send(":myself 004 %s :baz" % nick)
 
             while True:
-                data = (yield from self.reader.readline())
-                if not data:
-                    continue
-                print("%s: %s" % (id(self), data))
+                data = yield from self.get_next_line()
+                self.debug(data)
 
-                command, data = data.split(b" ", 1)
+                command, data = data.split(" ", 1)
 
-                if command == b"PING":
-                    self.writer.write(b"PONG " + data + b"\r\n")
+                if command == "PING":
+                    self.send("PONG " + data)
 
         except Exception as e:
             import traceback, sys
             traceback.print_exc(file=sys.stdout)
-            print(e)
+            self.debug(e)
+
+    @asyncio.coroutine
+    def get_next_line(self):
+        data = (yield from self.reader.readline())
+        while not data:
+            data = (yield from self.reader.readline())
+
+        self.debug(data.decode("Utf-8"))
+        return data.decode("Utf-8")
+
+    def debug(self, data):
+        if isinstance(data, bytes):
+            print("%s -> %s" % (self.nick if self.nick else id(self), data.decode("Utf-8").rstrip()))
+        else:
+            print("%s -> %s" % (self.nick if self.nick else id(self), data.rstrip()))
 
     def send(self, data):
         if isinstance(data, bytes):
             if not data.endswith(b"\r\n"):
                 data += b"\r\n"
 
+            print("%s <- %s" % (self.nick, data.decode("Utf-8").rstrip()))
             self.writer.write(data)
 
         else:
             if not data.endswith("\r\n"):
                 data += "\r\n"
 
+            print("%s <- %s" % (self.nick, data.rstrip()))
             self.writer.write(data.encode("Utf-8"))
 
 
